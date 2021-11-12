d7345cfc756fb0ebdf5c9ed34dea0162071f0ba8
giacoletti <giacoletti92@gmail.com>
2021-11-12 23:57:24 +0100

    Formatting code with Rufo, changing unit test for paint method of Car class fixing unit test to check if a car has a driver

--- cars/car.rb
+++ cars/car.rb
@@ -1,9 +1,7 @@
-class Car 
+class Car
+  attr_accessor :color, :driver
 
-    attr_accessor :color, :wheels, :max_speed
-
-    def paint(new_color)
-        @color = new_color
-    end
-
+  def paint(new_color)
+    @color = new_color
+  end
 end
--- cars/spec/car_spec.rb
+++ cars/spec/car_spec.rb
@@ -1,18 +1,18 @@
-require './car.rb'
+require "./car.rb"
 
 describe Car do
+  let(:driver) { instance_double("Driver", name: "Giovanni") }
 
-    let(:driver) { instance_double('Driver', name: 'Giovanni')}
+  subject { described_class.new }
 
-    subject { described_class.new }
+  it "is expected to change :color attribute using paint method" do
+    subject.color = 'blue'
+    expect { subject.paint('orange') }
+      .to change { subject.color }.from('blue').to('orange')
+  end
 
-    it 'is expected to change :color attribute using paint method' do
-        subject.color = 'blue'
-        expect(subject.paint('orange')).to eq subject.color
-    end
-
-    it 'is expected to have a driver' do
-        expect(driver).to be instance_of?
-    end
-
+  it "is expected to have a driver with :name 'Giovanni'" do
+    subject.driver = driver
+    expect(subject.driver.name).to eq 'Giovanni'
+  end
 end
